a3d3136dcfc9464486a1887b3cd820fc9e062d48
Martín Rubio <martinrubio23@gmail.com>
2017-07-21 11:26:53 +0100

    Fix/empty results (#10)
    * Remove coverage temporaly
    
    * [B] Fix error on empty result. Now return empty array
    
    * Comment solving

--- src/auto-redis/autocomplete.ts
+++ src/auto-redis/autocomplete.ts
@@ -5,19 +5,22 @@ export default function autocomplete ({pzrank, pzrange, zset}: {pzrank:any, pzra
 
     const rank = await pzrank(zset, payload);
     const inc = 128;
-
-    async function internal(init: number, end: number, acc: string[] = []): Promise<string[]> {
-      const nextPage = await pzrange(zset, init, end);
-      const results = [
-        ...acc,
-        ...nextPage.filter((gd: string) => gd.startsWith(payload) && gd.charAt(gd.length -1) === '*')
-                  .map((gd: string) => gd.substring(0, gd.length -1))
-      ];
-      const last = nextPage.pop();
-      if (last && last.startsWith(payload)) {
-        return internal(init + inc, end + inc, results);
+    async function internal(start: number, end: number, acc: string[] = []): Promise<string[]> {
+      if(start) {
+        const nextPage = await pzrange(zset, start, end);
+        const results = [
+          ...acc,
+          ...nextPage.filter((gd: string) => gd.startsWith(payload) && gd.charAt(gd.length -1) === '*')
+                    .map((gd: string) => gd.substring(0, gd.length -1))
+        ];
+        const last = nextPage.pop();
+        if (last && last.startsWith(payload)) {
+          return internal(start + inc, end + inc, results);
+        } else {
+          return results;
+        }
       } else {
-        return results;
+        return [];
       }
     }
     return internal(rank, rank + inc);
