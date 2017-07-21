e3fc1d484bd56dd331b87648f10da9adfbf2f3f6
Martin Rubio <martinrubio23@gmail.com>
2017-07-21 10:51:24 +0100

    [B] Fix error on empty result. Now return empty array

--- src/auto-redis/autocomplete.ts
+++ src/auto-redis/autocomplete.ts
@@ -5,19 +5,23 @@ export default function autocomplete ({pzrank, pzrange, zset}: {pzrank:any, pzra
 
     const rank = await pzrank(zset, payload);
     const inc = 128;
-
+    console.log(rank);
     async function internal(init: number, end: number, acc: string[] = []): Promise<string[]> {
-      const nextPage = await pzrange(zset, init, end);
-      const results = [
-        ...acc,
-        ...nextPage.filter((gd: string) => gd.startsWith(payload) && gd.charAt(gd.length -1) === '*')
-                  .map((gd: string) => gd.substring(0, gd.length -1))
-      ];
-      const last = nextPage.pop();
-      if (last && last.startsWith(payload)) {
-        return internal(init + inc, end + inc, results);
-      } else {
-        return results;
+      if(init) {
+        const nextPage = await pzrange(zset, init, end);
+        const results = [
+          ...acc,
+          ...nextPage.filter((gd: string) => gd.startsWith(payload) && gd.charAt(gd.length -1) === '*')
+                    .map((gd: string) => gd.substring(0, gd.length -1))
+        ];
+        const last = nextPage.pop();
+        if (last && last.startsWith(payload)) {
+          return internal(init + inc, end + inc, results);
+        } else {
+          return results;
+        }
+      }else {
+        return [];
       }
     }
     return internal(rank, rank + inc);
