3a544fa19667e53169fa912da82beab5e7bc9a85
Martin Rubio <martinrubio23@gmail.com>
2017-07-24 17:10:54 +0100

    Coverage for service.ts WIP

--- src/service.spec.ts
+++ src/service.spec.ts
@@ -33,22 +33,32 @@ test('Testing basic service', (t: Test) => {
     t.ok(_iris.register.called, 'Add from iris is called');
 
 
-    let addCall = _iris.register.getCall(0);
-    const statusImp = addCall.args[0].handler;
-    t.equal(addCall.args[0].pattern, 'status.muninn', 'The service exposes a status handle');
-    addCall = _iris.register.getCall(3);
-    t.equal(addCall.args[0].pattern, 'action.disease.autocomplete', 'The service exposes a disease handle');
-    addCall = _iris.register.getCall(4);
-    t.equal(addCall.args[0].pattern, 'action.assay.autocomplete', 'The service exposes a assay handle');
-    addCall = _iris.register.getCall(5);
-    t.equal(addCall.args[0].pattern, 'action.tissue.autocomplete', 'The service exposes a tissue handle');
-    addCall = _iris.register.getCall(6);
-    t.equal(addCall.args[0].pattern, 'action.technology.autocomplete', 'The service exposes a technology handle');
+    const  statusCall = _iris.register.getCall(0);
+    const statusImp = statusCall.args[0].handler;
+    t.equal(statusCall.args[0].pattern, 'status.muninn', 'The service exposes a status handle');
 
-    const impResultP = statusImp({});
-    t.ok(impResultP instanceof Promise, 'The implementation of status returns a promise');
+    const diseaseCall = _iris.register.getCall(3);
+    const diseaseImp = diseaseCall.args[0].handler;
+    t.equal(diseaseCall.args[0].pattern, 'action.disease.autocomplete', 'The service exposes a disease handle');
+    const assayCall = _iris.register.getCall(4);
+    t.equal(assayCall.args[0].pattern, 'action.assay.autocomplete', 'The service exposes a assay handle');
+    const tissueCall = _iris.register.getCall(5);
+    t.equal(tissueCall.args[0].pattern, 'action.tissue.autocomplete', 'The service exposes a tissue handle');
+    const techCall = _iris.register.getCall(6);
+    t.equal(techCall.args[0].pattern, 'action.technology.autocomplete', 'The service exposes a technology handle');
+    const autoAllCall = _iris.register.getCall(7);
+    const autoAllImp = autoAllCall.args[0].handler;
+    t.equal(autoAllCall.args[0].pattern, 'action.autocomplete', 'The service exposes a update handle');
 
-    await impResultP
+    const createCall = _iris.register.getCall(8);
+    const createImp = createCall.args[0].handler;
+    t.equal(createCall.args[0].pattern, 'event.dataset.create', 'The service exposes a update handle');
+
+
+    const impResultStatus = statusImp({});
+    t.ok(impResultStatus instanceof Promise, 'The implementation of status returns a promise');
+
+    await impResultStatus
       .then((result: any) => {
         t.deepEqual(_pack, result, 'The implementation returns what we expect');
         t.ok(true, 'Implementation does not blow up');
@@ -56,8 +66,40 @@ test('Testing basic service', (t: Test) => {
       .catch(() => {
         t.notOk(true, 'Implementation should not blow up');
       });
-  }
 
+    const promiseImpDisease = diseaseImp({payload:'L'});
+    t.ok(promiseImpDisease instanceof Promise, 'The implementation of disease.autocomplete returns a promise');
+
+    const promiseAutoAll = autoAllImp({payload:'T'});
+    t.ok(promiseAutoAll instanceof Promise, 'The implementation of action.autocomplete returns a promise');
+
+    const promiseAutoAllNo = autoAllImp({});
+
+    const properPayload = {payload:{'properties':{'attributes':{'disease': 'cancer'}}}};
+    const noProper = {payload:{'properties':{'attributes':{'god': 'cancer'}}}};
+    const noAtri = {payload:{'properties':{'disease': 'cancer'}}};
+    const promiseCreate = createImp(properPayload);
+    t.ok(promiseCreate instanceof Promise, 'The implementation of event.dataset.create returns a promise');
+/*
+    await promiseCreate().then( (results:any) => {
+      t.ok(true, 'Create do not blow up with correct payload');
+      console.log(results);
+    }).catch ( (error: Error) => {
+      console.log(error);
+      t.notOk(true, 'Implementation with correct payload should not blow up');
+    });
+*/
+    await createImp(noProper).then( (results:any) => {
+      t.ok(true, 'Create do not blow up with no correct property in the payload');
+      console.log(results);
+    });
+    await createImp(noAtri).then( (results:any) => {
+      t.ok(true, 'Create do not blow up with no atribute field in the payload');
+      console.log(results);
+    });
+
+
+  }
   _test()
     .then(() => t.end())
     .catch(console.error);
