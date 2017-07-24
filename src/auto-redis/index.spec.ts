465063f16ea8147b4310fdf84cb7b9e96b6f0d7f
Martin Rubio <martinrubio23@gmail.com>
2017-07-24 15:23:26 +0100

    Coverage for index.ts WIP

---
+++ src/auto-redis/index.spec.ts
@@ -0,0 +1,36 @@
+import * as test from 'tape';
+import { Test } from 'tape';
+import { stub } from 'sinon';
+import init from '../service';
+import setupRedis from '.';
+
+test('Testing Status Handler', (t: Test) => {
+  async function _test() {
+    const _promisify = stub().returns(Promise.resolve());
+    const zcard = stub();
+    const zadd = stub();
+    const zrange = stub();
+    const keys = stub();
+    const _createClient = stub().returns({zcard, zadd, zrange, keys});
+    const _setupRedis = setupRedis({_promisify, _createClient});
+
+    t.equals(typeof _setupRedis, 'object', 'Module expose a function');
+
+    const redis = await _setupRedis;
+    /*
+    const autocomplete = stub();
+    const updateIndex = stub();
+    const statusZsets = stub();
+    const name = stub();
+    t.deepEquals( await _setupRedis,{'name': 'redis', 'autocomplete':autocomplete,'updateIndex':updateIndex, 'statusZsets': statusZsets},'Setup return the correct object');
+    */
+
+    const zset = 'test';
+    const _autocomplete = redis.autocomplete(zset);
+
+    t.equals(typeof _autocomplete, 'function','Redis.autocomplete returns a function');
+  }
+  _test()
+    .then(() => t.end())
+    .catch(console.error);
+});
